b9eff76da4cdd78cd0d5ff18543b7d8cab3a5a22
sanumauryaupc-cloud <sanumauryaupc@gmail.com>
2026-02-06 10:46:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 from os import getenv
 
-API_ID = int(os.environ.get("API_ID", ""))  # Replace "123456" with your actual api_id or use .env
-API_HASH = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+API_ID = int(os.environ.get("API_ID", "38528250"))  # Replace "123456" with your actual api_id or use .env
+API_HASH = os.environ.get("API_HASH", "2ed087f9139515b3fd05d63072f56672")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7803387854:AAFTcsui4jvkWdhWIYNtB2B2dZGegSTyO8g")
 
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))  # Your Telegram user ID
+OWNER_ID = int(os.environ.get("OWNER_ID", "7803387854"))  # Your Telegram user ID
 SUDO_USERS = list(map(int, os.environ.get("SUDO_USERS", "").split()))  # Space-separated user IDs
 
 MONGO_URL = os.environ.get("MONGO_URL", "")##your mongo url eg: withmongodb+srv://xxxxxxx:xxxxxxx@clusterX.xxxx.mongodb.net/?retryWrites=true&w=majority
